0eee6b761ebc9ce4018975f51079bb535a76ba32
Juan Wajnerman <jwajnerman@manas.com.ar>
2012-06-12 09:54:53 -0300

    Symlink data directory to shared during deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,10 @@ namespace :deploy do
       run "ln -nfs #{shared_path}/#{file}.yml #{release_path}/config/"
     end
   end
+
+  task :symlink_data, :roles => :app do
+    run "ln -nfs #{shared_path}/data #{release_path}/"
+  end
 end
 
 namespace :foreman do
@@ -58,6 +62,7 @@ end
 before "deploy:start", "deploy:migrate"
 before "deploy:restart", "deploy:migrate"
 after "deploy:update_code", "deploy:symlink_configs"
+after "deploy:update_code", "deploy:symlink_data"
 
 after "deploy:update", "foreman:export"    # Export foreman scripts
 after "deploy:restart", "foreman:restart"   # Restart application scripts
