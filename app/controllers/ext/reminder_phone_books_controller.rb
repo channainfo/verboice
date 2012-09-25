409c492a4df3fd1940fda5d7792016c48ff70a58
channa ly <chen.webdev@gmail.com>
2012-09-25 16:58:46 +0700

    add delete action

--- app/controllers/ext/reminder_phone_books_controller.rb
+++ app/controllers/ext/reminder_phone_books_controller.rb
@@ -22,7 +22,7 @@ module Ext
  		load_project
  		@reminder = @project.reminder_phone_books.build(params[:ext_reminder_phone_book])
  		if 	@reminder.save
- 			flash[:success] = "Successfully created"
+ 			flash[:notice] = "Successfully created"
  			redirect_to :action => "index", :project => @project
  		else		
  			render :new
@@ -38,7 +38,21 @@ module Ext
  	end
 
  	def destroy
+ 		load_project
+ 		begin
+ 			@reminder = @project.reminder_phone_books.find(params[:id])
+	 		if @reminder.delete
+	 			flash[:notice] = " Record : #{@reminder.name} has been deleted"
+	 		else	
+	 			flash[:error] = "Failed to delete"
+	 		end
+ 		rescue Exception => e
+ 			flash[:error] = e.message
+ 		ensure	
+ 			redirect_to :action => :index , :project => @project
+ 		end
 
+ 		
  	end
 
  end
