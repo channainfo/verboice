54c752f8e844a1dadd0e66c05bdcde1a371f2941
channa ly <chen.webdev@gmail.com>
2012-09-26 09:37:44 +0700

    build associations inside its own model preventing to touch the core

--- app/models/ext/ext_active_record.rb
+++ app/models/ext/ext_active_record.rb
@@ -2,5 +2,14 @@ module Ext
 	class ExtActiveRecord < ActiveRecord::Base
 	  self.abstract_class = true
 	  self.table_name_prefix = 'ext_'
+
+
+	  def self.has_many_reverse class_name, associats, options = {}
+	  	 class_name.constantize.instance_eval do
+	  	 	has_many associats, options
+	  	 end
+	  end
+
+
 	end
 end
--- app/models/ext/reminder_phone_book.rb
+++ app/models/ext/reminder_phone_book.rb
@@ -2,6 +2,8 @@ module Ext
 	class ReminderPhoneBook < ExtActiveRecord
 		belongs_to :project, :class_name => "::Project"
 
+		has_many_reverse "Project" ,:ext_reminder_phone_books, :class_name => "Ext::ReminderPhoneBook"
+
 		validates :name, :phone_number, :presence => true
 		validates :phone_number, :uniqueness => true
 
