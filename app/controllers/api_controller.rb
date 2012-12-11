d50508cf3af3810f4b26a5664828dd579455d412
Sokmesa KHIEV <mesa@instedd.org>
2012-12-11 09:29:49 +0700

    fix on variable injection in I18n

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,7 +4,7 @@ class ApiController < ApplicationController
 
   def errors_to_json(model_object, action)
     attrs = {
-      :summary => I18n.t("controllers.api_controller.problem_action"),
+      :summary => I18n.t("controllers.api_controller.problem_action", :action => action, :model_name => model_object.class.model_name),
       :properties => []
     }
     model_object.errors.each do |name, value|
