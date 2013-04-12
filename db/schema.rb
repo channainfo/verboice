eb14b00ae25447ee3f89a6dcc897ecddecab0cfb
Sokmesa KHIEV <mesa@instedd.org>
2013-04-12 09:52:17 +0700

    fix on delete reminder group message

--- db/schema.rb
+++ db/schema.rb
@@ -343,7 +343,6 @@ ActiveRecord::Schema.define(:version => 20130411023300) do
     t.integer  "call_flow_id"
     t.string   "time_zone"
     t.text     "variables"
-    t.string   "session_id"
   end
 
   add_index "queued_calls", ["call_flow_id"], :name => "index_queued_calls_on_call_flow_id"
