ac83434bbd47cd567ebc0e51c5df059fbee9274c
Chheang Kakada <kakada@instedd.org>
2013-04-12 16:47:23 +0700

    remove column reminder_phone_book_type_id from reminder schedules

---
+++ db/migrate/20130412080434_remove_column_reminder_phone_book_type_from_reminder_schedules.rb
@@ -0,0 +1,27 @@
+class RemoveColumnReminderPhoneBookTypeFromReminderSchedules < ActiveRecord::Migration
+  def up
+    # migrate reminder_phone_book_type to reminder_group
+    Ext::ReminderPhoneBookType.all.each do |type|
+      addresses = type.reminder_phone_books.map &:phone_number
+      reminder_group = Ext::ReminderGroup.create(name: type.name, project_id: type.project_id, addresses: addresses)
+      Ext::ReminderSchedule.where(reminder_phone_book_type_id: type.id).each do |schedule|
+        schedule.reminder_group = reminder_group
+        schedule.save
+      end
+    end
+
+    remove_column :ext_reminder_schedules, :reminder_phone_book_type_id
+  end
+
+  def down
+    add_column :ext_reminder_schedules, :reminder_phone_book_type_id, :integer
+
+    # migrate reminder_phone_book_type to reminder_group
+    Ext::ReminderPhoneBookType.all.each do |type|
+      reminder_groups = Ext::ReminderGroup.where(name: type.name, project_id: type.project_id)
+      reminder_groups.each do |reminder_group|
+        reminder_group.destroy
+      end
+    end
+  end
+end
