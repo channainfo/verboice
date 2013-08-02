51e6b904e9ae4ba1954b412cd0150689acdf1d21
Chheang Kakada <kakada@instedd.org>
2013-08-02 10:51:25 +0700

    allow underscore for fusion table name text field

--- app/assets/javascripts/call_flow.js
+++ app/assets/javascripts/call_flow.js
@@ -2,6 +2,7 @@ $(function() {
   $('#call_flow_fusion_table_name').keypress(function(e){
     if (e.which > 0 // check that key code exists
       && e.which != 8 // allow backspace
+      && e.which != 95 // allow underscore
       // && e.which != 32 && e.which != 45 //allow space and dash 
       && !(e.which >= 48 && e.which <= 57) // allow 0-9
       && !(e.which >= 65 && e.which <= 90) // allow A-Z
