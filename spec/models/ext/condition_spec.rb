1e7fa4324990c446e467c03d95f8dbe9a9a22300
Chheang Kakada <kakada@instedd.org>
2013-08-22 16:59:19 +0700

    add project_id to Ext::Condition model

--- spec/models/ext/condition_spec.rb
+++ spec/models/ext/condition_spec.rb
@@ -8,10 +8,11 @@ describe Ext::Condition do
   end
 
   it "should build array from hash values" do
-    hash = {"0" => {variable: 'testing', operator: '=', value: '5', data_type: 'number'} }
+    hash = {"0" => {project_id: 1, variable: 'testing', operator: '=', value: '5', data_type: 'number'} }
     array = Ext::Condition.build hash
     array.size.should eq 1
     array.first.class.should eq Ext::Condition
+    array.first.project_id.should eq 1
     array.first.variable.should eq 'testing'
     array.first.operator.should eq '='
     array.first.data_type.should eq 'number'
@@ -21,13 +22,14 @@ describe Ext::Condition do
   describe "#evaluate?" do
     before(:each) do
       @contact = Contact.make
-      @project_variable = ProjectVariable.make :name => "var1"
+      @project = Project.make
+      @project_variable = ProjectVariable.make :name => "var1", project_id: @project.id
     end
 
     it "should return true when project variable doesn't exists" do
       PersistedVariable.make(contact_id: @contact.id, project_variable_id: @project_variable.id, value: "5")
       persisted_variables = @contact.persisted_variables
-      condition = Ext::Condition.new "var2", "=", "5", 'number'
+      condition = Ext::Condition.new @project.id, "var2", "=", "5", 'number'
 
       condition.evaluate?(persisted_variables).should be true
     end
@@ -37,7 +39,7 @@ describe Ext::Condition do
         it "should return true when it's match" do
           PersistedVariable.make(contact_id: @contact.id, project_variable_id: @project_variable.id, value: "5")
           persisted_variables = @contact.persisted_variables
-          condition = Ext::Condition.new "var1", "=", "5", 'number'
+          condition = Ext::Condition.new @project.id, "var1", "=", "5", 'number'
 
           condition.evaluate?(persisted_variables).should be true
         end
@@ -45,7 +47,7 @@ describe Ext::Condition do
         it "should return false when it's not match" do
           PersistedVariable.make(contact_id: @contact.id, project_variable_id: @project_variable.id, value: "5")
           persisted_variables = @contact.persisted_variables
-          condition = Ext::Condition.new "var1", ">", "5", 'number'
+          condition = Ext::Condition.new @project.id, "var1", ">", "5", 'number'
 
           condition.evaluate?(persisted_variables).should be false
         end
@@ -53,7 +55,7 @@ describe Ext::Condition do
         it "should return false when persisted variable value is date time" do
           PersistedVariable.make(contact_id: @contact.id, project_variable_id: @project_variable.id, value: "2013-03-20")
           persisted_variables = @contact.persisted_variables
-          condition = Ext::Condition.new "var1", "=", "5", 'number'
+          condition = Ext::Condition.new @project.id, "var1", "=", "5", 'number'
 
           condition.evaluate?(persisted_variables).should be false
         end
@@ -73,7 +75,7 @@ describe Ext::Condition do
           it "should return false" do
             PersistedVariable.make(contact_id: @contact.id, project_variable_id: @project_variable.id, value: "5")
             persisted_variables = @contact.persisted_variables
-            condition = Ext::Condition.new "var1", "=", "5", 'day'
+            condition = Ext::Condition.new @project.id, "var1", "=", "5", 'day'
 
             condition.evaluate?(persisted_variables).should be false
           end
@@ -94,31 +96,31 @@ describe Ext::Condition do
           end
 
           it "should return true when persisted variable has value is equal to 2 day ago" do
-            condition = Ext::Condition.new "var1", "=", "2", 'day'
+            condition = Ext::Condition.new @project.id, "var1", "=", "2", 'day'
 
             condition.evaluate?(@persisted_variables).should be true
           end
 
           it "should return false when persisted variable has no value is greater than 2 day ago" do
-            condition = Ext::Condition.new "var1", ">", "2", 'day'
+            condition = Ext::Condition.new @project.id, "var1", ">", "2", 'day'
 
             condition.evaluate?(@persisted_variables).should be false
           end
 
           it "should return true when persisted variable value is greater or equal to 2 day ago" do
-            condition = Ext::Condition.new "var1", ">=", "2", 'day'
+            condition = Ext::Condition.new @project.id, "var1", ">=", "2", 'day'
 
             condition.evaluate?(@persisted_variables).should be true
           end
 
           it "should return false when persisted variable has no value is less than 2 day ago" do
-            condition = Ext::Condition.new "var1", "<", "2", 'day'
+            condition = Ext::Condition.new @project.id, "var1", "<", "2", 'day'
 
             condition.evaluate?(@persisted_variables).should be false
           end
 
           it "should return true when persisted variable has value is less or equal to 2 day ago" do
-            condition = Ext::Condition.new "var1", "<=", "2", 'day'
+            condition = Ext::Condition.new @project.id, "var1", "<=", "2", 'day'
 
             condition.evaluate?(@persisted_variables).should be true
           end
@@ -139,31 +141,31 @@ describe Ext::Condition do
           end
 
           it "should return true when persisted variable has value is equal to 1 week ago" do
-            condition = Ext::Condition.new "var1", "=", "1", 'week'
+            condition = Ext::Condition.new @project.id, "var1", "=", "1", 'week'
 
             condition.evaluate?(@persisted_variables).should be true
           end
 
           it "should return false when persisted variable has no value is greater than 1 week ago" do
-            condition = Ext::Condition.new "var1", ">", "1", 'week'
+            condition = Ext::Condition.new @project.id, "var1", ">", "1", 'week'
 
             condition.evaluate?(@persisted_variables).should be false
           end
 
           it "should return true when persisted variable value is greater or equal to 1 week ago" do
-            condition = Ext::Condition.new "var1", ">=", "1", 'week'
+            condition = Ext::Condition.new @project.id, "var1", ">=", "1", 'week'
 
             condition.evaluate?(@persisted_variables).should be true
           end
 
           it "should return false when persisted variable has no value is less than 1 week ago" do
-            condition = Ext::Condition.new "var1", "<", "1", 'week'
+            condition = Ext::Condition.new @project.id, "var1", "<", "1", 'week'
 
             condition.evaluate?(@persisted_variables).should be false
           end
 
           it "should return true when persisted variable has value is less or equal to 1 week ago" do
-            condition = Ext::Condition.new "var1", "<=", "1", 'week'
+            condition = Ext::Condition.new @project.id, "var1", "<=", "1", 'week'
 
             condition.evaluate?(@persisted_variables).should be true
           end
@@ -184,31 +186,31 @@ describe Ext::Condition do
           end
 
           it "should return true when persisted variable has value is equal to 1 month ago" do
-            condition = Ext::Condition.new "var1", "=", "1", 'month'
+            condition = Ext::Condition.new @project.id, "var1", "=", "1", 'month'
 
             condition.evaluate?(@persisted_variables).should be true
           end
 
           it "should return false when persisted variable has no value is greater than 1 month ago" do
-            condition = Ext::Condition.new "var1", ">", "1", 'month'
+            condition = Ext::Condition.new @project.id, "var1", ">", "1", 'month'
 
             condition.evaluate?(@persisted_variables).should be false
           end
 
           it "should return true when persisted variable value is greater or equal to 1 month ago" do
-            condition = Ext::Condition.new "var1", ">=", "1", 'month'
+            condition = Ext::Condition.new @project.id, "var1", ">=", "1", 'month'
 
             condition.evaluate?(@persisted_variables).should be true
           end
 
           it "should return false when persisted variable has no value is less than 1 month ago" do
-            condition = Ext::Condition.new "var1", "<", "1", 'month'
+            condition = Ext::Condition.new @project.id, "var1", "<", "1", 'month'
 
             condition.evaluate?(@persisted_variables).should be false
           end
 
           it "should return true when persisted variable has value is less or equal to 1 month ago" do
-            condition = Ext::Condition.new "var1", "<=", "1", 'month'
+            condition = Ext::Condition.new @project.id, "var1", "<=", "1", 'month'
 
             condition.evaluate?(@persisted_variables).should be true
           end
@@ -229,31 +231,31 @@ describe Ext::Condition do
           end
 
           it "should return true when persisted variable has value is equal to 1 year ago" do
-            condition = Ext::Condition.new "var1", "=", "1", 'year'
+            condition = Ext::Condition.new @project.id, "var1", "=", "1", 'year'
 
             condition.evaluate?(@persisted_variables).should be true
           end
 
           it "should return false when persisted variable has no value is greater than 1 year ago" do
-            condition = Ext::Condition.new "var1", ">", "1", 'year'
+            condition = Ext::Condition.new @project.id, "var1", ">", "1", 'year'
 
             condition.evaluate?(@persisted_variables).should be false
           end
 
           it "should return true when persisted variable value is greater or equal to 1 year ago" do
-            condition = Ext::Condition.new "var1", ">=", "1", 'year'
+            condition = Ext::Condition.new @project.id, "var1", ">=", "1", 'year'
 
             condition.evaluate?(@persisted_variables).should be true
           end
 
           it "should return false when persisted variable has no value is less than 1 year ago" do
-            condition = Ext::Condition.new "var1", "<", "1", 'year'
+            condition = Ext::Condition.new @project.id, "var1", "<", "1", 'year'
 
             condition.evaluate?(@persisted_variables).should be false
           end
 
           it "should return true when persisted variable has value is less or equal to 1 year ago" do
-            condition = Ext::Condition.new "var1", "<=", "1", 'year'
+            condition = Ext::Condition.new @project.id, "var1", "<=", "1", 'year'
 
             condition.evaluate?(@persisted_variables).should be true
           end
