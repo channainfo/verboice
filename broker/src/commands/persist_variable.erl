489fceac1fc01a98d2ae2e0ee15e52d704421046
Chheang Kakada <kakada@instedd.org>
2013-10-08 09:40:38 +0700

    Add date step command to new broker

--- broker/src/commands/persist_variable.erl
+++ broker/src/commands/persist_variable.erl
@@ -6,13 +6,23 @@
 run(Args, Session = #session{js_context = JS}) ->
   Name = proplists:get_value(name, Args),
   Expression = proplists:get_value(expression, Args),
+  Type = proplists:get_value(type, Args),
   {Value, JS2} = erjs:eval(Expression, JS),
 
-  PersistedVar = (find_or_create_persisted_variable(Name, Session))#persisted_variable{value = Value},
+  TypeBin = list_to_binary(Type),
+  NewValue = case TypeBin of
+    <<>> -> Value;
+    UnitBin ->
+      Today = erlang:date(),
+      NewDate = time_ago(Value, Today, UnitBin),
+      edate:date_to_string(NewDate)
+  end,
+
+  PersistedVar = (find_or_create_persisted_variable(Name, Session))#persisted_variable{value = NewValue},
   PersistedVar:save(),
 
   VarName = list_to_atom("var_" ++ Name),
-  JS3 = erjs_context:set(VarName, Value, JS2),
+  JS3 = erjs_context:set(VarName, NewValue, JS2),
   {next, Session#session{js_context = JS3}}.
 
 find_or_create_persisted_variable(Name, #session{contact = Contact, project = Project}) ->
@@ -29,3 +39,14 @@ find_or_create_persisted_variable(Name, #session{contact = Contact, project = Pr
         {project_variable_id, ProjectVarId}
       ])
   end.
+
+time_ago(NumberAgo, FromDate = {_, _, _}, UnitBin) ->
+  NewDate = case UnitBin of
+    <<>> -> erlang:localtime();
+    <<"Day">> -> edate:shift(FromDate, -list_to_integer(NumberAgo), day);
+    <<"Week">> -> edate:shift(FromDate, -list_to_integer(NumberAgo), week);
+    <<"Month">> -> edate:shift(FromDate, -list_to_integer(NumberAgo), month);
+    <<"Year">> -> edate:shift(FromDate, -list_to_integer(NumberAgo), year)
+  end,
+
+  NewDate.
