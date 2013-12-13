ec431ad5bc3d7749a9435aea2587afdf489167db
Chheang Kakada <kakada@instedd.org>
2013-12-13 09:38:42 +0700

    modify duration to allow null in call logs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131113042549) do
+ActiveRecord::Schema.define(:version => 20131213022401) do
 
   create_table "accounts", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
@@ -117,7 +117,7 @@ ActiveRecord::Schema.define(:version => 20131113042549) do
     t.string   "fail_reason"
     t.integer  "contact_id"
     t.string   "pbx_logs_guid"
-    t.integer  "duration",      :default => 0,        :null => false
+    t.integer  "duration",      :default => 0
     t.integer  "retries",       :default => 0,        :null => false
   end
 
@@ -228,7 +228,7 @@ ActiveRecord::Schema.define(:version => 20131113042549) do
   create_table "ext_reminder_schedules", :force => true do |t|
     t.string  "name"
     t.date    "start_date"
-    t.integer "schedule_type",       :default => 1
+    t.integer "schedule_type",       :default => 0
     t.integer "recursion"
     t.string  "days"
     t.integer "call_flow_id"
