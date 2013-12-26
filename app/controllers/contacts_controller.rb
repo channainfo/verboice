cdac6b791ce1be1f80221a3474885bb1e709a8fc
mann <chanmann@instedd.org>
2013-12-26 16:15:24 +0700

    show contact call history

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -19,6 +19,7 @@ class ContactsController < ApplicationController
   before_filter :authenticate_account!
   before_filter :load_project, :only => [:new, :create, :index, :invitable]
   before_filter :initialize_context, :only => [:show, :edit, :update, :destroy]
+  before_filter :call_logs, only: :edit
 
   def index
     @contacts = @project.contacts.includes(:addresses).includes(:recorded_audios).includes(:persisted_variables).includes(:project_variables)
@@ -140,5 +141,12 @@ class ContactsController < ApplicationController
         variable['_destroy'] = "1"
       end
     end if params[:contact][:persisted_variables_attributes].present?
+  end
+
+  def call_logs
+    @logs = current_account.call_logs.includes(:channel).includes(:call_flow)
+    @logs = @logs.where address: @contact.first_address
+    @logs = @logs.order 'id DESC'
+    @logs = @logs.paginate :page => params[:page]
   end
 end
