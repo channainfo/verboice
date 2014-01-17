c1303b48aed66f24aab71d0b058ee60e5f19bffe
Chheang Kakada <kakada@instedd.org>
2014-01-17 16:45:48 +0700

    add delete cascading reminder channels when channels has been removed

--- app/models/ext/reminder_channel.rb
+++ app/models/ext/reminder_channel.rb
@@ -3,6 +3,8 @@ module Ext
   	attr_accessible :reminder_schedule_id, :channel_id
     belongs_to :reminder_schedule, class_name: 'Ext::ReminderSchedule'
     belongs_to :channel, class_name: 'Channel'
+
+    assign_has_many_to "Channel", :ext_reminder_channels, :class_name => "Ext::ReminderChannel", :dependent => :destroy
   end
 end
 
