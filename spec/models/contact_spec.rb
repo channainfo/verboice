6ef24df9068868dba5e7052040aebc402ff47d32
mann <chanmann@instedd.org>
2014-01-24 15:50:58 +0700

    fix broken tests

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,39 +2,38 @@ require 'spec_helper'
 
 describe Contact do
   describe "#evaluate?" do
-    before(:each) do
-      @contact = Contact.make
-      @project_var1 = ProjectVariable.make :name => "var1"
-      @project_var2 = ProjectVariable.make :name => "var2"
-    end
+    let(:project) { Project.make }
+    let(:contact) { Contact.make project: project }
+    let(:project_var1) { project.project_variables.make :name => "var1" }
+    let(:project_var2) { project.project_variables.make :name => "var2" }
 
     it "should return true when all the project variables do not exists" do
       conditions = [Ext::Condition.new("var_undefined", "=", "5", "number")]
 
-      @contact.evaluate?(conditions).should be true
+      contact.evaluate?(conditions).should be true
     end
 
     it "should return true when it's match all conditions" do
-      PersistedVariable.make(contact_id: @contact.id, project_variable_id: @project_var1.id, value: "5")
-      PersistedVariable.make(contact_id: @contact.id, project_variable_id: @project_var2.id, value: "10")
+      PersistedVariable.make(contact_id: contact.id, project_variable_id: project_var1.id, value: "5")
+      PersistedVariable.make(contact_id: contact.id, project_variable_id: project_var2.id, value: "10")
       conditions = [Ext::Condition.new("var1", "=", "5", "number"), Ext::Condition.new("var2", ">", "5", "number")]
 
-      @contact.evaluate?(conditions).should be true
+      contact.evaluate?(conditions).should be true
     end
 
     it "should return false when it's not match at least one condition" do
-      PersistedVariable.make(contact_id: @contact.id, project_variable_id: @project_var1.id, value: "5")
-      PersistedVariable.make(contact_id: @contact.id, project_variable_id: @project_var2.id, value: "10")
+      PersistedVariable.make(contact_id: contact.id, project_variable_id: project_var1.id, value: "5")
+      PersistedVariable.make(contact_id: contact.id, project_variable_id: project_var2.id, value: "10")
       conditions = [Ext::Condition.new("var1", "=", "5", "number"), Ext::Condition.new("var2", ">", "10", "number")]
 
-      @contact.evaluate?(conditions).should be false
+      contact.evaluate?(conditions).should be false
     end
 
     it "should return true when some project variables do not exists and the rest of the project variables are matches condition" do 
-      PersistedVariable.make(contact_id: @contact.id, project_variable_id: @project_var1.id, value: "5")
+      PersistedVariable.make(contact_id: contact.id, project_variable_id: project_var1.id, value: "5")
       conditions = [Ext::Condition.new("var1", "=", "5", "number"), Ext::Condition.new("var_undefined", "=", "10", "number")]
 
-      @contact.evaluate?(conditions).should be true
+      contact.evaluate?(conditions).should be true
     end
   end
 
