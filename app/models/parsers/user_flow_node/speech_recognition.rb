080c7afe7ee0d8203cea501fd224c92a4cd88d33
Channa Ly <channa.info@gmail.com>
2014-02-12 17:45:51 +0700

    speech recognition step

---
+++ app/models/parsers/user_flow_node/speech_recognition.rb
@@ -0,0 +1,65 @@
+# Copyright (C) 2010-2012, InSTEDD
+#
+# This file is part of Verboice.
+#
+# Verboice is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Verboice is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Verboice.  If not, see <http://www.gnu.org/licenses/>.
+
+module Parsers
+  module UserFlowNode
+    class SpeechRecognition < UserCommand
+      attr_reader :id, :name, :call_flow
+      attr_accessor :next
+
+      def initialize call_flow, params
+        @id = params['id']
+        @name = params['name'] || ''
+        @explanation_resource = Resource.new params['explanation_resource']
+        @confirmation_resource = Resource.new params['confirmation_resource']
+        @timeout = params['timeout']
+        @stop_key = params['stop_key']
+        @call_flow = call_flow
+        @next = params['next']
+        @root_index = params['root']
+
+        @old_persisted_variable_name = params['old_store']
+        @persisted_variable_name = params['store']
+      end
+
+      def is_root?
+        @root_index.present?
+      end
+
+      def root_index
+        @root_index
+      end
+
+      def equivalent_flow
+        Compiler.parse do |compiler|
+          compiler.Label @id
+          compiler.AssignValue "current_step", @id
+          compiler.AssignValue "current_step_name", "#{@name}"
+          compiler.Trace context_for %("Record message. Download link: " + record_url(#{@id}))
+          compiler.append @explanation_resource.equivalent_flow
+          compiler.SpeechRecognition @id, @name,{:stop_keys => @stop_key,
+                                                 :timeout => @timeout, 
+                                                 :old_var_name => @old_persisted_variable_name, 
+                                                 :var_name => @persisted_variable_name}
+
+          compiler.append @confirmation_resource.equivalent_flow
+          compiler.append @next.equivalent_flow if @next
+        end
+      end
+    end
+  end
+end
---
+++ broker/src/commands/speech_recognition.erl
@@ -0,0 +1,87 @@
+-module(speech_recognition).
+-export([run/2]).
+-include("session.hrl").
+-include("db.hrl").
+
+run(Args, Session = #session{pbx = Pbx, call_log = CallLog, contact = Contact, project = Project}) ->
+  Key = util:to_string(proplists:get_value(key, Args)),
+  Description = proplists:get_value(description, Args),
+  OldVarName = proplists:get_value(old_var_name, Args),
+  VarName = proplists:get_value(var_name, Args),
+  StopKeys = proplists:get_value(stop_keys, Args, "01234567890*#"),
+  Timeout = proplists:get_value(timeout, Args, 10),
+
+  CallLog:info("Record user voice", [{command, "record"}, {action, "start"}]),
+  CallLogId = CallLog:id(),
+  Filename = filename(CallLogId, Key),
+  filelib:ensure_dir(Filename),
+
+  Pbx:record(Filename, StopKeys, Timeout),
+
+  RecordedAudio = #recorded_audio{
+    contact_id = Contact#contact.id,
+    project_id = Project#project.id,
+    call_log_id = CallLogId,
+    key = Key,
+    description = Description
+  },
+  RecordedAudio:save(),
+
+  create_call_log_recorded_audio(OldVarName, VarName, Key, Description, Project#project.id, CallLogId),
+
+  CallLog:info("Speech translation", [{command, "speech_recognition"}, {action, "start"}]),
+  % TODO text translation
+  CallLog:info("Speech translation", [{command, "speech_recognition"}, {action, "finish"}]),
+
+
+  CallLog:info("Recording saved", [{command, "speech_recognition"}, {action, "finish"}]),
+  {next, Session}.
+
+filename(CallLogId, Key) ->
+  filename:join(["../data/call_logs/", util:to_string(CallLogId), "results", Key ++ ".wav"]).
+
+create_call_log_recorded_audio(OldVarName, VarName, Key, Description, ProjectId, CallLogId) ->
+  OldVarNameBin = list_to_binary(OldVarName),
+  VarNameBin = list_to_binary(VarName),
+  if
+    OldVarNameBin /= <<>>; VarNameBin /= <<>> ->
+      ProjectVariableOld = project_variable:find([{project_id, ProjectId}, {name, OldVarName}]),
+      ProjectVariableCur = project_variable:find([{project_id, ProjectId}, {name, VarName}]),
+
+      ProjectVariable = case ProjectVariableOld  of
+        undefined -> 
+          if 
+            ProjectVariableCur == undefined ->    
+              NewProjectVariable = #project_variable{project_id = ProjectId, name = VarName},
+              NewProjectVariable:save() ;
+            true -> 
+              ProjectVariableCur      
+          end;  
+        _ -> 
+          if OldVarName  /= OldVarNameBin -> ProjectVariableOld:update([{name, VarName}]) end,
+          ProjectVariableOld
+      end,
+
+      ProjectVariableId = ProjectVariable#project_variable.id ,
+
+      CallLogRecordedAudio = call_log_recorded_audio:find([ {call_log_id, CallLogId}, 
+                                                            {project_variable_id, ProjectVariableId }
+                                                          ]),
+      case CallLogRecordedAudio of
+        undefined ->
+          NewCallLogRecordedAudio = #call_log_recorded_audio{
+            call_log_id = CallLogId,
+            project_variable_id = ProjectVariableId,
+            key = Key,
+            description = Description
+          },
+          NewCallLogRecordedAudio:save();
+        _ -> 
+          CallLogRecordedAudio:update([ {call_log_id, CallLogId}, 
+                                        {project_variable_id, ProjectVariableId}, 
+                                        {key, Key}, 
+                                        {description, Description}
+                                      ])
+      end;
+    true -> io:format("Do nothing", [])
+  end.
