a18da89146da136644e03ec250f2477aa1308104
Mann <chanmannlim@gmail.com>
2014-02-13 15:57:12 +0700

    refactor add store_call_log_entries to project migration

--- db/migrate/20140213073041_add_store_call_log_entries_to_project.rb
+++ db/migrate/20140213073041_add_store_call_log_entries_to_project.rb
@@ -1,9 +1,5 @@
 class AddStoreCallLogEntriesToProject < ActiveRecord::Migration
-  def up
+  def change
     add_column :projects, :store_call_log_entries, :boolean, default: true
-  end
-
-  def down
-    remove_column :projects, :store_call_log_entries
   end
 end
