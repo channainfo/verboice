8f819c07a5ccf72abe290129655283a5b4847dd2
Kakada Chheang <kakada.chheang@gmail.com>
2014-02-21 10:30:39 +0700

    fix render csv download

--- spec/helpers/call_log_helper_spec.rb
+++ spec/helpers/call_log_helper_spec.rb
@@ -3,27 +3,29 @@ require 'spec_helper'
 describe CallLogHelper do
   describe '.render_csv_download?' do
     before(:each) do
+      @project_id = 1
       @logs = double('logs', count: 0)
+      query = double('query', search: @logs)
       @audios = double('audios', count: 2)
-      CallLog.should_receive(:search).and_return(@logs)
+      CallLog.should_receive(:where).with(project_id: @project_id).and_return(query)
       CallLogRecordedAudio.should_receive(:where).with(call_log_id: @logs).and_return(@audios)
     end
 
     it "should be false" do
-      helper.render_csv_download?.should eq false
+      helper.render_csv_download?(@project_id).should eq false
     end
 
     context "when over CSV_MAX_ROWS" do
       before { @logs.stub(count: 270000) }
       it "should be true" do
-        helper.render_csv_download?.should eq true
+        helper.render_csv_download?(@project_id).should eq true
       end
     end
 
     context "when no recorded audios" do
       before { @audios.stub(count: 0) }
       it "should be true" do
-        helper.render_csv_download?.should eq true
+        helper.render_csv_download?(@project_id).should eq true
       end
     end
   end
