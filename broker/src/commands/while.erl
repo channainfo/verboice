a66235964b520c44f0068b3456dd6a9ad9fcacf8
Channa Ly <channa.info@gmail.com>
2014-03-14 17:52:38 +0700

    working in erlang for looping

--- broker/src/commands/while.erl
+++ broker/src/commands/while.erl
@@ -4,7 +4,10 @@
 
 run(Args, Session = #session{js_context = JS}) ->
   Condition = proplists:get_value(condition, Args),
+  io:format("~n ----------------------while condition ~p", [Condition]),
   {Value, JS2} = erjs:eval(Condition, JS),
+
+  io:format("~n ---------------------- while value: ~p", [Value]),
 
   Action = case Value of
     true ->
